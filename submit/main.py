e832bd29cf7506f02099decb58425eaaa04416c8
Yuan Zhang <mike.zy7@gmail.com>
2020-11-06 09:24:38 +0000

    run locally

--- submit/main.py
+++ submit/main.py
@@ -1,4 +1,5 @@
 
+import tensorflow.compat.v1 as tf
 
 # customized policy networks here: cnn, impala_cnn
 from gfootball.examples import models  
@@ -11,7 +12,7 @@ from baselines.common.policies import build_policy
 from baselines.common.tf_util import get_session, save_variables, load_variables
 
 # parameters
-level = "11_vs_11_easy_stocahstic"
+level = "11_vs_11_easy_stochastic"
 state_representation = "extracted_stacked"
 reward_experiment = "scoring,checkpoints"
 network = "impala_cnn"
@@ -31,6 +32,8 @@ env = football_env.create_environment(
 # get state_space and action_space
 ob_space = env.observation_space
 ac_space = env.action_space
+print("state space, action space")
+print(ob_space, ac_space)
 
 
 # define actor policy
@@ -40,14 +43,28 @@ sess = tf.Session(config=config)
 policy_fn = build_policy(env, network)
 policy = policy_fn(nbatch=1, sess=sess)
 
+prefix = "ppo2_model/"
 # load checkpoints from file
-load_variables(load_path, sess)
+import joblib, os
+variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+
+loaded_params = joblib.load(os.path.expanduser(load_path))
+
+restores = []
+if isinstance(loaded_params, list):
+    assert len(loaded_params) == len(variables), 'number of variables loaded mismatches len(variables)'
+    for d, v in zip(loaded_params, variables):
+        restores.append(v.assign(d))
+else:
+    for v in variables:
+        v_name = prefix + v.name 
+        restores.append(v.assign(loaded_params[v_name]))
+sess.run(restores)
+print("policy loaded")
+
 
 
 # main function for agent
 def agent(obs):
-    actions, values, _, _ = model.step(obs)
+    actions, values, _, _ = policy.step(obs)
     return actions
-
-
-
