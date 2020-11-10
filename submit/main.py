32658acf47aebfc9c095ce8441b364514483ae77
Yuan Zhang <mike.zy7@gmail.com>
2020-11-10 06:35:23 +0000

    fix

--- submit/main.py
+++ submit/main.py
@@ -1,15 +1,18 @@
 
 import sys
-# sys.path.append('/kaggle_simulations/agent/')
-sys.path.append('./')
+sys.path.append('/kaggle_simulations/agent/')
+# sys.path.append('./')
+import os
+os.environ['CUDA_DEVCIE_ORDER'] = "PCR_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.policies import ActorCriticPolicy
 
 from env_wrapper import GFootballEnv
 
-# load_dir = "/kaggle_simulations/agent/ppo_gfootball.pt"
-load_dir = "ppo_gfootball.pt"
+load_dir = "/kaggle_simulations/agent/ppo_gfootball.pt"
+# load_dir = "ppo_gfootball.pt"
 class EnvArgs(object):
     level = '11_vs_11_easy_stochastic'
     state = 'extracted_stacked'
@@ -26,7 +29,6 @@ policy = ActorCriticPolicy
 # agent
 model = PPO(policy, eval_env)
 model.load(load_dir)
-model.cpu()
 print("Agent loaded.")
 
 # almost same as env_wrapper/gfootball.py when state="extracted_stacked"
